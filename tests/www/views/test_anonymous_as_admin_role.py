c2422d767a68a160f44d4b13bb2581a236a2bec8
sudipto baral <sudiptobaral.me@gmail.com>
2024-03-20 20:08:54 -0400

    fix: adapt unit test as connexion_app.client no longer return 302.
    Signed-off-by: sudipto baral <sudiptobaral.me@gmail.com>

--- tests/www/views/test_anonymous_as_admin_role.py
+++ tests/www/views/test_anonymous_as_admin_role.py
@@ -17,8 +17,6 @@
 # under the License.
 from __future__ import annotations
 
-from urllib.parse import quote_plus
-
 import pytest
 
 from airflow.models import Pool
@@ -55,8 +53,10 @@ def pool_factory(session):
 def test_delete_pool_anonymous_user_no_role(anonymous_client, pool_factory):
     pool = pool_factory()
     resp = anonymous_client.post(f"pool/delete/{pool.id}")
-    assert 302 == resp.status_code  # TODO: this returns 200 now
-    assert f"/login/?next={quote_plus(f'http://localhost/pool/delete/{pool.id}')}" == resp.headers["Location"]
+    assert 200 == resp.status_code
+    # Verify that the pool still exists.
+    with create_session() as session:
+        assert session.query(Pool).filter(Pool.id == pool.id).one()
 
 
 def test_delete_pool_anonymous_user_as_admin(anonymous_client_as_admin, pool_factory):
